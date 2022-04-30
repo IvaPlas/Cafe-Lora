5ab64ff1a3080828f5c8a6fd2ef535e69cd90d8f
Ivana Plašienková <ivana.plasienkova@gmail.com>
2022-04-30 12:37:11 +0200

    Order button set up

--- src/index.js
+++ src/index.js
@@ -15,3 +15,21 @@ navigace.addEventListener('click', handleNav);
 polozkyNavigace.forEach((item) => {
   item.addEventListener('click', handleNav);
 });
+
+let ordered = false;
+
+const orderButtonElm = document.querySelector('.order-btn');
+const cupElm = document.querySelector('.drink__cup');
+
+const zrusit = () => {
+  if (ordered === false) {
+    orderButtonElm.textContent = 'Zrušit';
+    cupElm.classList.add('drink__cup--selected');
+    ordered = true;
+  } else {
+    orderButtonElm.textContent = 'Objednat';
+    cupElm.classList.remove('drink__cup--selected');
+    ordered = false;
+  }
+};
+orderButtonElm.addEventListener('click', zrusit);
