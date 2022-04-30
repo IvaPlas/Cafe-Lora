ce0cdaf5393b759222b3331ff53192b87c8aa118
Ivana Plašienková <ivana.plasienkova@gmail.com>
2022-04-30 11:39:48 +0200

    Hiding and popping up navigation bar in mobile

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navigace = document.querySelector('#nav-btn');
+const nav = document.querySelector('nav');
+const polozkyNavigace = document.querySelectorAll('a');
+
+const handleNav = () => {
+  nav.classList.toggle('nav-closed');
+};
+
+navigace.addEventListener('click', handleNav);
+
+polozkyNavigace.forEach((item) => {
+  item.addEventListener('click', handleNav);
+});
